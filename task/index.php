3e9416cc03096bb98b9b5b077218f3ffe05206f2
laminellas <159630004+laminellas@users.noreply.github.com>
2024-09-17 16:02:54 +0100

    postalcode.php

--- task/index.php
+++ task/index.php
@@ -1,58 +1,29 @@
 <?php
+ini_set('display_error', 'on');
+error_reporting(E_ALL);
+
+$executionStartTime = microtime(true);
+$url='http://api.geonames.org/findNearbyPostalCodes?&lat=' . $_REQUEST['lat'] . '&long=' . $_REQUEST['long'] .'&username=alamine';
 
 
-
-// Get the API type from the form submission
-$apiType = $_POST['apiType'];
-
-// Determine which API to call based on the 'apiType' field
-switch ($apiType) {
-    case 'postalCode':
-        // Handle Postal Code Lookup
-        $postalCode = $_POST['postalcode'];
-        $country = $_POST['country'];
-        $url = "http://api.geonames.org/findNearbyPostalCodesJSON?postalcode=$postalCode&country=$country&username=alamine";
-        break;
-
-    case 'wikipedia':
-        // Handle Find Nearby Wikipedia Entries
-        $lat = $_POST['lat'];
-        $lng = $_POST['lng'];
-        $url = "http://api.geonames.org/findNearbyWikipediaJSON?lat=$lat&lng=$lng&username=alamine";
-        break;
-
-    case 'timezone':
-        // Handle Time Zone Lookup
-        $lat = $_POST['timezoneLat'];
-        $lng = $_POST['timezoneLng'];
-        $url = "http://api.geonames.org/timezoneJSON?lat=$lat&lng=$lng&username=alamine";
-        break;
-
-    default:
-        echo "Invalid API type.";
-        exit();
-}
-
-// Initialize cURL session
 $ch = curl_init();
-
-// Set the URL and other cURL options
-curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url);
 
-// Execute the cURL request and fetch the response
-$response = curl_exec($ch);
+$result=curl_exec($ch);
 
-// Close the cURL session
 curl_close($ch);
 
-// Return the API response
-if ($response) {
-    echo "<pre>";
-    print_r(json_decode($response, true));
-    echo "</pre>";
-} else {
-    echo "No data returned or an error occurred.";
-}
+$decode = json_decode($result, true);
 
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returndIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+$output['data'] = $decode['geonames'];
+
+header('Content-Type: application/json; chartset=UTF-8');
+
+echo json_encode($output);
 ?>
