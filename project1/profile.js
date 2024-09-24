0495fe82a5c54bdf15b1535a58e126c31a67efe8
laminellas <159630004+laminellas@users.noreply.github.com>
2024-09-24 13:05:52 +0100

    Update profile.js

--- project1/profile.js
+++ project1/profile.js
@@ -1,1 +1,11 @@
+<?php
+$filename = 'C:\new\htdocs\project1\countryBorders.geo.json';
+$content = file_get_contents($filename);
 
+if ($content === false) {
+    echo "Error reading the file";
+} else {
+     
+    echo json_encode($content);
+}
+?>
