5aff55386cc9bc6114472a441ae0af7022671e42
laminellas <159630004+laminellas@users.noreply.github.com>
2024-09-27 15:45:05 +0100

    Update script.js

--- project1/script.js
+++ project1/script.js
@@ -1,5 +1,4 @@
-
-  $.ajax({
+$.ajax({
     url: "profile.php",
     type: 'GET',
     dataType: 'json',
@@ -8,17 +7,62 @@
        const $dropdown = $('#countryDropdown'); 
 
        
-       $.each(result, function(index, country) {
+       $.each(result.data, function(index, country) {
            $dropdown.append(
-               $('#drop')
-                   .val(result.data[index].iso_a2) 
-                   .text(result.data[index].name) 
-                );
+              $('<option></option>')
+
+                 .val(result.data[index].iso_a2)
+
+                 .text(result.data[index].name)
+
+         );
        })
-   },error:function(jqXHR){
+   },error:function(jqXHR, textStatus, errorThrown){
 
       console.log(jqXHR);
 
   }
   
 }); 
+
+var map;
+
+// tile layers
+
+var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
+    attribution: "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"
+  }
+);
+
+var satellite = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
+    attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+  }
+);
+
+var basemaps = {
+  "Streets": streets,
+  "Satellite": satellite
+};
+
+// buttons
+
+var infoBtn = L.easyButton("fa-info fa-xl", function (btn, map) {
+  $("#exampleModal").modal("show");
+});
+
+
+$(document).ready(function () {
+  
+  map = L.map("map", {
+    layers: [streets]
+  }).setView([54.5, -4], 6);
+  
+  // setView is not required in your application as you will be
+  // deploying map.fitBounds() on the country border polygon
+
+  layerControl = L.control.layers(basemaps).addTo(map);
+
+  infoBtn.addTo(map);
+
+})
+
