2c262cbf663ea016b1a5b48b013c2f204dac209a
laminellas <159630004+laminellas@users.noreply.github.com>
2024-10-01 08:46:11 +0100

    Update script.js

--- project1/script.js
+++ project1/script.js
@@ -25,19 +25,19 @@ $.ajax({
   
 }); 
 
+// ---------------------------------------------------------
+// GLOBAL DECLARATIONS
+// ---------------------------------------------------------
 var map;
 
 // tile layers
-
 var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
     attribution: "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"
-  }
-);
+});
 
 var satellite = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
     attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-  }
-);
+});
 
 var basemaps = {
   "Streets": streets,
@@ -45,48 +45,54 @@ var basemaps = {
 };
 
 // buttons
-
 var infoBtn = L.easyButton("fa-info fa-xl", function (btn, map) {
   $("#exampleModal").modal("show");
 });
 
+// ---------------------------------------------------------
+// EVENT HANDLERS
+// ---------------------------------------------------------
 
+// initialise and add controls once DOM is ready
 $(document).ready(function () {
   
+  // Create the map object and set the default tile layer
   map = L.map("map", {
     layers: [streets]
-  }).setView([54.5, -4], 6);
+  }).setView([54.5, -4], 6); // Initial view (centered on UK, change as needed)
   
-  
-
+  // Add layer control for basemaps
   layerControl = L.control.layers(basemaps).addTo(map);
-
+  
+  // Add the info button
   infoBtn.addTo(map);
 
-})
+  // ---------------------------------------------------------
+  // GET THE DEVICE LOCATION USING GEOLOCATION API
+  // ---------------------------------------------------------
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        // Success callback: user location available
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
 
-//currencyExchange API
-$(document).ready(function(){
-  $('#convert').on('click', function(){
-      var toCurrency = $('#currency').val();
-      $.ajax({
-          url: 'currencyExchange.php', 
-          type: 'GET',
-          data: {
-              to_currency: toCurrency
-          },
-          success: function(response){
-              var data = JSON.parse(response);
-              console.log(result);
-              if(data.success) {
-                  $('#result').html('1 USD = ' + data.rate + ' ' + data.currency);
-              } else {
-                  $('#result').html('Error: ' + data.message);
-              }
-          },error:function(jqXHR, textStatus, errorThrown){
-
-      console.log(jqXHR);
-
-      });
-  });
+        // Center the map at the user's location
+        map.setView([lat, lng], 13); // Zoom level 13 to focus on the current location
+        
+        // Add a marker at the user's current location
+        L.marker([lat, lng]).addTo(map)
+          .bindPopup("You are here!")
+          .openPopup();
+      },
+      function (error) {
+        // Error callback: handle any error
+        console.error("Geolocation failed: " + error.message);
+        alert("Unable to retrieve your location.");
+      }
+    );
+  } else {
+    // Geolocation is not supported by the browser
+    alert("Geolocation is not supported by your browser.");
+  }
 });
