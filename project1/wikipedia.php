f1f0b73fe657858e801400db9e476f912aae07c2
laminellas <159630004+laminellas@users.noreply.github.com>
2024-10-07 10:48:42 +0100

    Update wikipedia.php

--- project1/wikipedia.php
+++ project1/wikipedia.php
@@ -1,1 +1,27 @@
+<?php
+// getWikipediaLinks.php
 
+// GeoNames username (replace with your actual username)
+$username = 'alamine';
+
+// Get country ID from the request
+$countryId = $_GET['countryId'];
+
+// Fetch information about the country
+$url = "http://api.geonames.org/getJSON?geonameId=$countryId&username=$username";
+
+$response = file_get_contents($url);
+$data = json_decode($response, true);
+
+// Extract Wikipedia URL if available
+$wikipediaLink = isset($data['geoname']['wikipediaUrl']) ? $data['geoname']['wikipediaUrl'] : null;
+
+if ($wikipediaLink) {
+    echo "<h3>Wikipedia Links</h3>";
+    echo "<ul>";
+    echo "<li><a href='$wikipediaLink' target='_blank'>$wikipediaLink</a></li>";
+    echo "</ul>";
+} else {
+    echo "No Wikipedia link found for this country.";
+}
+?>
