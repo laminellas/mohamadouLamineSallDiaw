cc108839c4da4b44442b9cdbe5644690379c0bd4
laminellas <159630004+laminellas@users.noreply.github.com>
2024-10-08 09:26:45 +0100

    Update getCountries.php

--- project1/getCountries.php
+++ project1/getCountries.php
@@ -1,1 +1,20 @@
+<?php
+header('Content-Type: application/json');
 
+// Fetch country data
+$url = "https://restcountries.com/v3.1/all"; // API endpoint
+$response = file_get_contents($url);
+$countries = json_decode($response, true);
+
+// Format the data for the dropdown
+$countryList = [];
+foreach ($countries as $country) {
+    $countryList[] = [
+        'name' => $country['name']['common'],
+        'alpha2Code' => $country['cca2']
+    ];
+}
+
+// Output JSON
+echo json_encode($countryList);
+?>
