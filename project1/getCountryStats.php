ebe0cf89810ac557f3850ec166d89056521474e2
laminellas <159630004+laminellas@users.noreply.github.com>
2024-10-08 09:27:45 +0100

    Update getCountryStats.php

--- project1/getCountryStats.php
+++ project1/getCountryStats.php
@@ -1,1 +1,31 @@
+<?php
+header('Content-Type: application/json');
+
+// Get country code from request
+$countryCode = isset($_GET['countryCode']) ? $_GET['countryCode'] : '';
+
+if ($countryCode) {
+    // Fetch country data from REST Countries API
+    $url = "https://restcountries.com/v3.1/alpha/{$countryCode}"; // API endpoint
+    $response = file_get_contents($url);
+    $countryData = json_decode($response, true);
+
+    if (!empty($countryData)) {
+        $countryInfo = $countryData[0];
+        $result = [
+            'name' => $countryInfo['name']['common'],
+            'population' => $countryInfo['population'],
+            'area' => $countryInfo['area'],
+            'region' => $countryInfo['region'],
+            'subregion' => $countryInfo['subregion'],
+            'timezones' => $countryInfo['timezones']
+        ];
+        echo json_encode($result);
+    } else {
+        echo json_encode(null);
+    }
+} else {
+    echo json_encode(null);
+}
+?>
 
